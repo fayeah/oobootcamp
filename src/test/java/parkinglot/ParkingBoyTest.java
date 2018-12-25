b92bed53b21a4d4eede709e01720212b81e409e5
qyfei <qyfei@thoughtworks.com>
2018-12-25 23:39:24 +0800

    task3: parking boy parked failed when all parking lots are full

--- src/test/java/parkinglot/ParkingBoyTest.java
+++ src/test/java/parkinglot/ParkingBoyTest.java
@@ -2,6 +2,7 @@ package parkinglot;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -33,5 +34,26 @@ class ParkingBoyTest {
 
     assertNotNull(receipt);
     assertSame(myCar, parkingLot2.locateCar(receipt));
+  }
+
+  @Test
+  void shouldParkFailedWhenAllParkingLotAreFull() {
+    Car myCar = new Car();
+    Car car1 = new Car();
+    Car car2 = new Car();
+    Car car3 = new Car();
+    ParkingLot parkingLot1 = new ParkingLot(1);
+    ParkingLot parkingLot2 = new ParkingLot(1);
+    ParkingLot parkingLot3 = new ParkingLot(1);
+
+    ParkingBoy parkingBoy = new ParkingBoy();
+
+    car1.park(parkingLot1);
+    car2.park(parkingLot2);
+    car3.park(parkingLot3);
+
+    assertThrows(ParkingSpacesAreFullException.class, () -> parkingBoy.park(myCar, parkingLot1));
+    assertThrows(ParkingSpacesAreFullException.class, () -> parkingBoy.park(myCar, parkingLot2));
+    assertThrows(ParkingSpacesAreFullException.class, () -> parkingBoy.park(myCar, parkingLot3));
   }
 }
